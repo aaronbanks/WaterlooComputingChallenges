ee312245d9d35b07ea959a36f3dfe372547ff618
aaronbanks <aaronbanks@aaronbanks.ca>
2021-12-22 14:06:18 -0500

    go

---
+++ 2005/S1_J2.py
@@ -0,0 +1,26 @@
+def main ():
+    lower_limit = int(input("Enter lower limit of range:"))
+    upper_limit = int(input("Enter upper limit of range:")) + 1
+
+    rsa_numbers_in_range = 0
+
+    for number in range(lower_limit, upper_limit):
+        if rsa_check(number) == True:
+            rsa_numbers_in_range += 1
+
+    print(f"The number of RSA number between {lower_limit} and {upper_limit - 1} is {rsa_numbers_in_range}")
+
+def rsa_check(number):
+    total_even_divisors = 0
+
+    for numbers in range(1, number + 1):
+        if number % numbers == 0:
+            total_even_divisors += 1
+
+    if total_even_divisors == 4:
+        return True
+
+    else:
+        return False
+
+main()
---
+++ 2005/S1_J3.py
@@ -0,0 +1,36 @@
+def main():
+
+    with open('S1_J3_INPUT.txt') as input_file:
+        input_lines = input_file.readlines()
+
+
+        inverted_line = ""
+        location = ""
+        print_line = False
+
+        for line in reversed(input_lines):
+
+            clean_line = line.strip("\n")
+
+            if clean_line == "SCHOOL":
+                continue
+
+            if clean_line != "L" and clean_line != "R":
+                location = clean_line
+
+            if print_line == True:
+                print(f"Turn {inverted_line} onto {location} street.")
+                print_line = False
+
+            if clean_line == "R":
+                inverted_line = "LEFT"
+                print_line = True
+
+            elif clean_line == "L":
+                inverted_line = "RIGHT"
+                print_line = True
+
+
+        print(f"Turn {inverted_line} into your HOME.")
+
+main()
