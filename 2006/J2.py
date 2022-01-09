62cd47e7170565456c7739d91af50aa3f340c6bd
aaronbanks <aaronbanks@aaronbanks.ca>
2022-01-09 14:44:28 -0500

    go

---
+++ 2006/J2.py
@@ -0,0 +1,16 @@
+def main():
+    number_of_ways_to_ten = 0
+
+    sides_on_die_m = int(input("Enter m:"))
+
+    sides_on_die_n = int(input("Enter n:"))
+
+    for sides in range(1, sides_on_die_m + 1):
+        for sidez in range(1, sides_on_die_n + 1):
+
+            if sides + sidez == 10:
+                number_of_ways_to_ten += 1
+    
+    print(f"There are {number_of_ways_to_ten} ways too get the sum 10.")
+
+main()
