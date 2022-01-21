380ec4f59a44c7c0d4094ad10b2432c56acec75a
aaronbanks <aaronbanks@aaronbanks.ca>
2022-01-21 15:35:48 -0500

    go

--- 2007/J5.py
+++ 2007/J5.py
@@ -1,6 +1,21 @@
 def main():
 
-    set_of_motel_locations = {0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000}
+    set_of_motel_locations = {
+        0,
+        990,
+        1010,
+        1970,
+        2030,
+        2940,
+        3060,
+        3930,
+        4060,
+        4970,
+        5030,
+        5990,
+        6010,
+        7000,
+    }
 
     minimum_to_travel = int(input())
 
@@ -12,31 +27,51 @@ def main():
         set_of_motel_locations.add(int(input()))
 
     distance_traveled = 0
+    route_taken = []
 
-    possible_routes = route_finder(distance_traveled, minimum_to_travel, maximum_to_Travel, set_of_motel_locations)
+    possible_routes = route_finder(
+        distance_traveled,
+        minimum_to_travel,
+        maximum_to_Travel,
+        set_of_motel_locations,
+        route_taken,
+    )
 
     print(f"Output: {possible_routes}")
 
-def route_finder(distance, mini, maxi, motels):
+
+def route_finder(distance, mini, maxi, motels, route_taken):
     possible_routes_found = 0
 
     while distance < 7000:
         option_found = False
 
+        route_taken_copy = route_taken.copy()
+
         for motel in motels:
             if motel >= (distance + mini) and motel <= (distance + maxi):
 
                 if option_found == True:
-                    possible_routes_found += route_finder(motel, mini, maxi, motels)
+                    possible_routes_found += route_finder(
+                        motel, mini, maxi, motels, route_taken_copy
+                    )
 
                 option_found = True
 
+                route_taken.append(motel)
+                print(f"{motel}")
+
                 if motel == 7000:
                     possible_routes_found += 1
-                distance = motel
+                    print(f"Route taken: {route_taken}")
+
+                new_distance = motel
+        distance = new_distance
 
         if option_found == False:
             return 0
 
     return possible_routes_found
+
+
 main()
