824d2fd59194c64bfc9bcc9a1227db51a598b15b
aaronbanks <aaronbanks@aaronbanks.ca>
2022-01-21 15:00:37 -0500

    go

--- 2007/J5.py
+++ 2007/J5.py
@@ -9,32 +9,34 @@ def main():
     number_of_motels_added = int(input())
 
     for motel in range(number_of_motels_added):
-        set_of_motel_locations.add(input())
-
-    number_of_possible_routes = 0
+        set_of_motel_locations.add(int(input()))
 
     distance_traveled = 0
-    possible_routes = 0
-    set_of_routes = {}
 
-    current_trip = []
+    possible_routes = route_finder(distance_traveled, minimum_to_travel, maximum_to_Travel, set_of_motel_locations)
 
+    print(f"Output: {possible_routes}")
 
-    def route_finder(distance, mini, maxi, motels, trip_so_far):
-        while distance < 7000:
-            option_found = False
-            for motel in motels:
-                if motel >= (distance + mini) and motel <= (distance + maxi):
-                    if option_found == True:
-                        route_finder(motel, mini, maxi, motels, trip_so_far.append(motel))
+def route_finder(distance, mini, maxi, motels):
+    possible_routes_found = 0
 
-                    distance = motel
-                    option_found = True
-                    trip_so_far.append(motel)
+    while distance < 7000:
+        option_found = False
 
+        for motel in motels:
+            if motel >= (distance + mini) and motel <= (distance + maxi):
 
+                if option_found == True:
+                    possible_routes_found += route_finder(motel, mini, maxi, motels)
 
+                option_found = True
 
+                if motel == 7000:
+                    possible_routes_found += 1
+                distance = motel
 
+        if option_found == False:
+            return 0
 
+    return possible_routes_found
 main()
