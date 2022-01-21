eb03d6e667e2cb81f817feec07ffdaa3594fc205
aaronbanks <aaronbanks@aaronbanks.ca>
2022-01-21 09:55:24 -0500

    go

---
+++ 2007/J1.py
@@ -0,0 +1,11 @@
+def main():
+
+    list_of_bowls = []
+
+    for bowl in range(3):
+        list_of_bowls.append(int(input()))
+
+    list_of_bowls.sort
+    print(f"{list_of_bowls[1]}")
+
+main()
---
+++ 2007/J2.py
@@ -0,0 +1,34 @@
+def main():
+
+    while True:
+        short_form = input("Enter phrase> ")
+
+        if short_form == "CU":
+            print("see you")
+        elif short_form == ":-)":
+            print("I'm happy")
+        elif short_form == ":-(":
+            print("I'm unhappy")  
+        elif short_form == ";-)":
+            print("wink")
+        elif short_form == ":-P":
+            print("stick out my tongue")
+        elif short_form == "(~.~)":
+            print("sleepy")
+        elif short_form == "TA":
+            print("totally awesome")          
+        elif short_form == "CCC":
+            print("Canadian Computing-Competition")
+        elif short_form == "CUS":
+            print("because")  
+        elif short_form == "TW":
+            print("thank-you")
+        elif short_form == "YW":
+            print("you're welcome")
+        elif short_form == "TTYL":
+            print("talk to you later")
+            break
+        else:
+            print(f"{short_form}")
+
+main()
