3c55540e80963343c610b3528864c9764bbb50bf
aaronbanks <aaronbanks@aaronbanks.ca>
2022-03-01 14:21:51 -0500

    go

--- 2010/J2.py
+++ 2010/J2.py
@@ -8,39 +8,6 @@ def main():
 
     steps = int(input())
 
-    class Student:
-        def __init__(
-            self,
-            name,
-            location,
-            step_counter,
-            direction_forward,
-            forward_move,
-            backwards_move,
-        ):
-            self.name = name
-            self.location = location
-            self.step_counter = step_counter
-            self.direction_forward = direction_forward
-            self.forward_move = forward_move
-            self.backwards_move = backwards_move
-
-        def take_a_step(self):
-            if self.direction_forward == True:
-                self.location += 1
-                self.step_counter += 1
-
-                if self.step_counter >= self.forward_move:
-                    self.direction_forward = not self.direction_forward
-                    self.step_counter = 0
-            else:
-                self.location -= 1
-                self.step_counter += 1
-
-                if self.step_counter >= self.backwards_move:
-                    self.direction_forward = not self.direction_forward
-                    self.step_counter = 0
-
     nikky = Student(
         "nikky",
         0,
@@ -68,6 +35,40 @@ def main():
         print("Tied")
     else:
         print("Byron")
+
+
+class Student:
+    def __init__(
+        self,
+        name,
+        location,
+        step_counter,
+        direction_forward,
+        forward_move,
+        backwards_move,
+    ):
+        self.name = name
+        self.location = location
+        self.step_counter = step_counter
+        self.direction_forward = direction_forward
+        self.forward_move = forward_move
+        self.backwards_move = backwards_move
+
+    def take_a_step(self):
+        if self.direction_forward == True:
+            self.location += 1
+            self.step_counter += 1
+
+            if self.step_counter >= self.forward_move:
+                self.direction_forward = not self.direction_forward
+                self.step_counter = 0
+        else:
+            self.location -= 1
+            self.step_counter += 1
+
+            if self.step_counter >= self.backwards_move:
+                self.direction_forward = not self.direction_forward
+                self.step_counter = 0
 
 
 main()
